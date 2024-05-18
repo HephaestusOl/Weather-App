03be35214a0298d5f53e85c420ffae764c189ac9
Bagrad Bugaev <bagradbugaev05@gmail.com>
2024-05-18 19:11:01 +0300

    ready-made layout + pop-up

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './Pages/Home/components/Home';
 import { MonthStatistics } from './Pages/MonthStatistics/components/MonthStatistics';
 import { Header } from './shared/Header/Header'
+import { Popup } from './shared/Popup/Popup';
 
 function App() {
   return (
-		<div className='container'>
-			<Header />
-			<Routes>
-				<Route path='/' element={<Home/>} />
-				<Route path='/month-statistics' element={<MonthStatistics/>} />
-			</Routes>
+		<div className='global-container'>
+			<Popup/>
+			<div className='container'>
+				<Header/>
+				<Routes>
+					<Route path='/' element={<Home/>} />
+					<Route path='/month-statistics' element={<MonthStatistics/>} />
+				</Routes>
+			</div>
 		</div>
 	)
 }
--- src/Pages/Home/components/Home.tsx
+++ src/Pages/Home/components/Home.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 import s from './Home.module.scss'
+import { ThisDay } from "./ThisDay/ThisDay";
+import { ThisDayInfo } from "./ThisDayInfo/ThisDayInfo";
+import { Days } from "./Days/Days";
 
 type Props={}
 
 export const Home = (props: Props) => {
-  return <div className={s.home}></div>
+  return (
+  <div className={s.home}>
+    <div className={s.wrapper}>
+      <ThisDay />
+      <ThisDayInfo />
+    </div>
+    <Days />
+  </div>)
 }
